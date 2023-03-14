886264b29bd40b50c2013c352d9e24ed8cf3bf05
mariafigueiredo21 <127613479+mariafigueiredo21@users.noreply.github.com>
2023-03-14 12:29:57 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -15,110 +15,116 @@ ui <- fluidPage(
     "", 
     
     tabPanel("Home",
-         
+             
              #ADD THINGS ON HOME PAGE HERE
-                 
+             
     ),
     
     tabPanel("Descriptive Statistics",
-             
-            sidebarLayout(
-              sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 100,
-                        value = 20),
-            radioButtons("tops", "Choose the Top Influencer you want to look at", 
-                         choices=c("Top 10", "Top 50", "All"),
-                         selected="Top 10"),
-            selectInput("countries",
-                        label= "Select which country you want to analyse", 
-                        choices=dt.influencers.combined.country$unique, 
-                        selected="United States of America",
-                        multiple=F)
-                 
-               ),
-               
-               # Show a plot of the generated distribution
-            mainPanel(
-            tabsetPanel(type = "tab",
-                        tabPanel("General Data", 
-                                 h3("Data Outlook"),
-                                 tableOutput("summary_data"),
-                                 h3("Top 10 Influencers"),
-                                 tableOutput("Top_10_influencers"),
-                                 h3("Top 10 Influencers Categories"),
-                                 tableOutput("Top_10_Categories"),
-                                 h3("Top 10 Country Engagement"),
-                                 tableOutput("Top_10_contry_engage")),
-                                 
-                        tabPanel("Summary Statistics",
-                                 plotOutput("distPlot"),
-                                 verbatimTextOutput("summary"),
-                                 plotOutput("engageplot"),
-                                 verbatimTextOutput("summaryenga")),
-                                 
-                        tabPanel("All about your Influencer"), 
-                                 
-                        tabPanel("Get more Specific")
-               )
-             )
-             
-    ),
     
-    tabPanel("Network Exploration",
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput("bins",
+                    "Number of bins:",
+                    min = 1,
+                    max = 100,
+                    value = 20),
+        radioButtons("tops", "Choose the Top Influencer you want to look at", 
+                     choices=c("Top 10", "All"),
+                     selected="Top 10"),
+        selectInput("countries",
+                    label= "Select which country you want to analyse", 
+                    choices=dt.influencers.combined.country$unique, 
+                    selected="United States of America",
+                    multiple=F)
+        
+        
+      ),
       
-      # Sidebar 
-      sidebarLayout(
-        sidebarPanel(
-         
-          h5("Here you can change the parameters of the network you would like to see."),
-         
-          radioButtons("network.key",
-                      label = "Type of Network",
-                      choices = c("Bipartite Network", "Projected Network")),
-         
-          selectizeInput("category.input", "Select the Category:",
-                        choices = unique(dt.influencers.new$Category)
-          ),
-         
-          sliderInput("top.followers.input", "Top Followers to Display:",
-                      min = 0, max = 1000, value = 0, step = 25),
-          
-          sliderInput("nfollowers.input", "Minimum Number of Followers (milions):",
-                     min = 0, max = 470, value = 0, step = 25),
-          
-          checkboxInput("no.connection.input", "Delete nodes which have no connection to other nodes", value = FALSE),
-         
-        ),
-       
-       mainPanel(
-          tabsetPanel(
-            tabPanel("Summary", htmlOutput("student.summary")
-                     
-            ),
-            
-            tabPanel("Your Network", htmlOutput("influencer.network"),
-                     #numericInput(inputId = "x", label = "Enter a number:", value = 1),
-                     
-                     # Define the output plot
-                     plotOutput(outputId = "sine_plot")
-                    #WRITE HERE FOR THE TAB YOUR NETWORK
-            ),
-            
-            tabPanel("Overall Network", htmlOutput("overall.network")
-                     #WRITE HERE FOR THE TAB OVERALL NETWORK
-                     
-            )
-          )
+      # Show a plot of the generated distribution
+      mainPanel(
+        tabsetPanel(type = "tab",
+                    tabPanel("General Data", 
+                             h3("Data Outlook"),
+                             tableOutput("summary_data"),
+                             h3("Top 10 Influencers"),
+                             tableOutput("Top_10_influencers"),
+                             h3("Top 10 Influencers Categories"),
+                             tableOutput("Top_10_Categories"),
+                             h3("Top 10 Country Engagement"),
+                             tableOutput("Top_10_contry_engage")),
+                    
+                    tabPanel("Summary Statistics",
+                             plotOutput("distPlot"),
+                             verbatimTextOutput("summary"),
+                             plotOutput("engageplot"),
+                             verbatimTextOutput("summaryenga")),
+                    
+                    tabPanel("All about your Influencer"), 
+                    
+                    tabPanel("Get more Specific"),
+                    
         )
       )
     )
-  ),
-  
-  
-  
+    ),
+               
+             
+             
+             tabPanel("Network Exploration",
+                      
+                      # Sidebar 
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          h5("Here you can change the parameters of the network you would like to see."),
+                          
+                          radioButtons("network.key",
+                                       label = "Type of Network",
+                                       choices = c("Bipartite Network", "Projected Network")),
+                          
+                          selectizeInput("category.input", "Select the Category:",
+                                         choices = unique(dt.influencers.new$Category)
+                          ),
+                          
+                          sliderInput("top.followers.input", "Top Followers to Display:",
+                                      min = 0, max = 1000, value = 0, step = 25),
+                          
+                          sliderInput("nfollowers.input", "Minimum Number of Followers (milions):",
+                                      min = 0, max = 470, value = 0, step = 25),
+                          
+                          checkboxInput("no.connection.input", "Delete nodes which have no connection to other nodes", value = FALSE),
+                          
+                        ),
+                        
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Summary", htmlOutput("student.summary")
+                                     
+                            ),
+                            
+                            tabPanel("Your Network", htmlOutput("influencer.network"),
+                                     #numericInput(inputId = "x", label = "Enter a number:", value = 1),
+                                     
+                                     # Define the output plot
+                                     plotOutput(outputId = "sine_plot")
+                                     #WRITE HERE FOR THE TAB YOUR NETWORK
+                            ),
+                            
+                            tabPanel("Overall Network", htmlOutput("overall.network")
+                                     #WRITE HERE FOR THE TAB OVERALL NETWORK
+                                     
+                            ),
+                          )
+                        )
+                      
+             )
+    )
+    
+    
+    
+  )
 )
 
 ui
